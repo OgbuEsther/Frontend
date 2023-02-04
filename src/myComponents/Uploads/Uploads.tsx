84925f73d615ce467abf054957af3a0ee405d543
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-03 18:42:18 -0800

    updating

--- src/myComponents/Uploads/Uploads.tsx
+++ src/myComponents/Uploads/Uploads.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 import {BiImageAdd} from "react-icons/bi"
+import axios from "axios";
 
 
 const Uploads = () =>{
+const [name , setName] = React.useState()
+const [price, setPrice] = React.useState()
+    const upload = async()=>{
+        await axios.post(`` , {name , price})
+    }
+
     return (
         <div>
             <Container>
                 <Wrapper>
-                    <h1>Uploads</h1>
+                    <h1>Upload new looks</h1>
+                    <Box>
+                        <BiImageAdd/>
+                    </Box>
+                    <input type="text" placeholder="Enter name" />
+                    <input type="text" placeholder="Enter price" />
                 </Wrapper>
             </Container>
         </div>
@@ -17,10 +29,36 @@ const Uploads = () =>{
 
 export default Uploads
 
-const Wrapper = styled.div`
+const Box = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 5px;
+    border: 1px solid black;
+    background-color: white;
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 30px;
+`
+
+const Wrapper = styled.form`
 width: 300px;
-height: 300px;
-box-shadow: 0px 0px 2px 1px rgba(0,0,0,0.5);
+height: 500px;
+box-shadow: 0px 0px 2px 1px rgba(0,0,0,0.1);
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+
+input{
+    width: 200px;
+    height: 30px;
+    margin-bottom: 20px;
+    outline-color: gray;
+
+}
     
 `
 
